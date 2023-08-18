fe28575623f3f77134b22b14ece4818780b12a5d
Ehizoba Ogbebor <Ehizoba.Ogbebor@Interswitchng.com>
2023-08-18 15:16:47 +0100

    Added UpperLowerCase and its Impl Class

---
+++ src/main/java/com/interswitch/test/UpperLowerCase.java
@@ -0,0 +1,23 @@
+package com.interswitch.test;
+
+public class UpperLowerCase {
+    public String wordToUpperCase(String word){
+        if (null !=word && !word.isEmpty()) {
+            String[] words = word.split(" ");
+            return words[0].toUpperCase();
+        }
+        throw new
+                IllegalArgumentException("Word cannot be empty");
+        }
+    public String wordToLowerCase(String word){
+        if (null !=word && !word.isEmpty()) {
+            String[] words = word.split(" ");
+            return words[0].toLowerCase();
+        }
+        throw new
+                IllegalArgumentException("Word cannot be empty");
+    }
+
+    }
+
+
